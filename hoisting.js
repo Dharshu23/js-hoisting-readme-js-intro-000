5f456488821238ede935ad745aeefa4d4669c1de
Priyadharshini Thirukonda Mohanlal <priyamohanlal23@gmail.com>
2021-04-05 18:35:51 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -11,9 +11,9 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-   var thisIsCrazy;
+   return thisIsCrazy();
 
-  thisIsCrazy = function (){
+  var thisIsCrazy = function (){
     console.log("hey!!!");
   }
 }
